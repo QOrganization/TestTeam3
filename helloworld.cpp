f36c2e9f918688162048d75b8c053f97e1888ab8
jlmorr <jbike@icarus2.usc.edu>
2016-11-17 13:47:28 -0800

    Update code

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,1 +1,11 @@
-// Need to add code
+#include <cstdlib>
+#include <iostream>
+
+// Avoid having to type std::
+using namespace std;
+
+int main(int argc, char *argv[]) {
+// Removed std:: and added !
+	cout << "Hello world!" << endl;
+	return 0;
+}
